03f46999efcec8511d371e84b189709e4e0797bf
Emmanuel Silva Xavier <emanuelsx@hotmail.com>
2015-03-30 22:22:54 -0300

    Atualizando repositório

--- PP_2015_1_03/src/principal/Cliente.java
+++ PP_2015_1_03/src/principal/Cliente.java
@@ -5,36 +5,63 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Cliente {
-    int codigo;
-    String nome;
-    String telefone;
-    Date dataNascimento;
-    String endereco;
+     private int codigo;
+     private String nome;
+     private String telefone;
+     private Date dataNascimento;
+     private String endereco;
     
     
-    Cliente(String nome,Date data){
+    
+    public Cliente(String nome,Date data){
         JOptionPane.showMessageDialog(null, "Criado");
         this.nome = nome;
         this.dataNascimento = data;
         System.out.print(this.nome + " dt: " + this.dataNascimento);
     }
     
-    boolean salvar(){
-        JOptionPane.showMessageDialog(null, "Cadastrando cliente");
-        return true;
+   public Cliente(){
+        
     }
+   
+  private void conectar(){
+       //code of connection
+   }
+   
+  private boolean validarCampo(){
+       return true;
+   }
     
-    boolean alterar(){
+  private void registrarLog(){
+       //code
+   }
+   
+  private void desconectar(){
+       //code
+   }
+   
+   
+  public boolean salvar(){
+        conectar();
+        validarCampo();
+        JOptionPane.showMessageDialog(null, "Cadastrando cliente");
+        registrarLog();
+        desconectar();
+        return true;
+   }
+    
+  public boolean alterar(){
+       
         JOptionPane.showMessageDialog(null, "Alterando cliente");
         return true;
     }
     
-    boolean excluir(){
+   public boolean excluir(){
         JOptionPane.showMessageDialog(null, "Excluindo cliente");
         return true;
     }
     
-    List<Cliente> pesquisa(String nome){
+   public List<Cliente> pesquisa(String nome){
         JOptionPane.showMessageDialog(null, "Cadastrando cliente");
         return null;
     }
@@ -42,4 +69,46 @@ public class Cliente {
     public void finalize(){
         JOptionPane.showMessageDialog(null, "Objeto destruído.");
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        
+        return nome.toUpperCase();
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
 }
--- PP_2015_1_03/src/principal/Main.java
+++ PP_2015_1_03/src/principal/Main.java
@@ -1,20 +1,27 @@
 package principal;
 
+import com.sun.media.sound.JARSoundbankReader;
 import java.awt.FlowLayout;
 import java.util.Date;
+import javax.imageio.spi.RegisterableService;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
                 
-        Cliente cliente = new Cliente("Emmanuel", new Date());
-        cliente = null;              
-        System.gc();
-        cliente = new Cliente("Emnsdfdf", new Date());
+        Cliente cliente = new Cliente();
+        cliente.setCodigo(1);
+        cliente.setDataNascimento(new Date());
+        cliente.setNome("Emmanuel");
+        cliente.salvar();
+
         
+
+       
         
     }
 }
